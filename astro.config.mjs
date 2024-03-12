8fda6cfaed7e5326ddd315517842cf8ff3eff1ba
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2024-03-12 23:11:43 +0530

    🔧 Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://file-sense.github.io",
+  site: "https://file-sense.github.io/docs",
   base: "/docs",
   integrations: [
     starlight({
